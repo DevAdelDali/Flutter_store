317b762a5984a9f742dd15e90696294b1a151343
DevAdelDali <aldalyadelsaleh2002@gmail.com>
2022-12-05 10:00:31 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+import 'package:store/controller/controller.dart';
+import 'package:store/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+Locale? locale;
+final MyController mycontroler = Get.find();
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      supportedLocales: const [
+        Locale('ar', ''),
+        Locale('en', ''),
+      ],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      title: 'Store',
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.store),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static final ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+   /* appBarTheme: const AppBarTheme(
+      color: Colors.teal,
+      iconTheme: IconThemeData(
+        color: Colors.white,
+
+      ),
+    ),*/
+    colorScheme: const ColorScheme.light(
+      primary: Colors.white,
+      onPrimary: Colors.white,
+      // primaryVariant: Colors.white38,
+      secondary: Colors.red,
+    ),
+    cardTheme: const CardTheme(
+      color: Colors.teal,
+    ),
+    iconTheme: const IconThemeData(
+      color: Colors.white54,
+    ),
+    textTheme: const TextTheme(
+
+        /* title: TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+      ),
+      subtitle: TextStyle(
+        color: Colors.white70,
+        fontSize: 18.0,
+      ),*/
+        ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.green,
+    appBarTheme: const AppBarTheme(
+      color: Colors.black,
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+    ),
+    colorScheme: const ColorScheme.light(
+      primary: Colors.black,
+      onPrimary: Colors.black,
+      // primaryVariant: Colors.black,
+      secondary: Colors.red,
+    ),
+    cardTheme: const CardTheme(
+      color: Colors.black,
+    ),
+    iconTheme: const IconThemeData(
+      color: Colors.white54,
+    ),
+    textTheme: const TextTheme(
+        /*title: TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+      ),
+      subtitle: TextStyle(
+        color: Colors.white70,
+        fontSize: 18.0,
+      ),*/
+        ),
+  );
+}
