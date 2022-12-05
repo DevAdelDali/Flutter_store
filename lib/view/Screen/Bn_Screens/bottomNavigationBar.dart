a21106578839305cf0b654072a76d81c959ae2db
DevAdelDali <aldalyadelsaleh2002@gmail.com>
2022-12-05 11:29:57 +0200

    Create Folders

---
+++ lib/view/Screen/Bn_Screens/bottomNavigationBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomScreen extends StatefulWidget {
+  const BottomScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BottomScreen> createState() => _BottomScreenState();
+}
+
+class _BottomScreenState extends State<BottomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
