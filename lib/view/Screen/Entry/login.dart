9f7a99ee7572577ca94aa08a9b40e4dd7c96d6ef
DevAdelDali <aldalyadelsaleh2002@gmail.com>
2022-12-08 20:27:41 +0200

    Finish Entry Directory

--- lib/view/Screen/Entry/login.dart
+++ lib/view/Screen/Entry/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:store/view/Widget/botton_custom.dart';
 import 'package:store/view/Widget/text_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,58 +13,138 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   bool _showPass = true;
 
-  late TextEditingController _emailControler;
-  late TextEditingController _passwordControler;
+  late TextEditingController _login_emailControler;
+  late TextEditingController _login_passwordControler;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _emailControler = TextEditingController();
-    _passwordControler = TextEditingController();
+    _login_emailControler = TextEditingController();
+    _login_passwordControler = TextEditingController();
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
 
-    _emailControler.dispose();
-    _passwordControler.dispose();
+    _login_emailControler.dispose();
+    _login_passwordControler.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          TextFieldCustom(
-            isShow: false,
-            hintTitel: "hintTitel",
-            errorText: "errorText",
-            prefixIcon: Icon(Icons.person, color: Colors.green),
-            textInputType: TextInputType.text,
-            controller: _emailControler,
-          ),
-          TextFieldCustom(
-              hintTitel: "hintTitel",
-              errorText: "errorText",
-              prefixIcon: Icon(Icons.person, color: Colors.green),
-              textInputType: TextInputType.text,
-              controller: _passwordControler,
-              isShow: _showPass,
-              safixIcon: IconButton(
-                  onPressed: () {
-                    print(_showPass);
-                    setState(() {
-                      _showPass = !_showPass;
-                    });
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 100.5,
+            ),
+            Image.asset(
+              "images/Logo-1.png",
+              height: 120,
+              width: 120,
+            ),
+            const SizedBox(
+              height: 120,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 45.0),
+              child: Column(
+                children: [
+                  TextFieldCustom(
+                      hintTitel: "Email",
+                      errorText: "Enter your Email",
+                      isHide: false,
+                      prefixIcon: const Icon(Icons.email, color: Colors.grey),
+                      textInputType: TextInputType.emailAddress,
+                      controller: _login_emailControler,
+                      safixIcon: const Icon(
+                        Icons.done,
+                        color: Colors.green,
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFieldCustom(
+                      hintTitel: "Password",
+                      errorText: "Enter your Password",
+                      isHide: _showPass,
+                      prefixIcon: const Icon(Icons.lock, color: Colors.grey),
+                      textInputType: TextInputType.visiblePassword,
+                      controller: _login_passwordControler,
+                      safixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _showPass = !_showPass;
+                            });
+                          },
+                          icon: Icon(
+                            _showPass
+                                ? Icons.remove_red_eye
+                                : Icons.visibility_off,
+                            color: const Color(0xff707070),
+                          )))
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/forget_pass_screen");
                   },
-                  icon: Icon(
-                    _showPass ? Icons.remove_red_eye : Icons.visibility_off,
-                    color: Colors.grey,
-                  )))
-        ],
+                  child: Text(
+                    "Forgot Password?",
+                    style: GoogleFonts.poppins(
+                        fontSize: 12,
+                        backgroundColor: Colors.transparent,
+                        fontWeight: FontWeight.bold,
+                        color: const Color(0xff586BCA)),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 39,
+            ),
+            BottonCustom(
+                titel: "Sign In",
+                colorBotton: const Color(0xff586BCA),
+                textColorBotton: Colors.white,
+                fontSize: 14,
+                height: 50,
+                width: 285,
+                onTap: () {}),
+            const SizedBox(
+              height: 140,
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, "/register");
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.transparent),
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+                ),
+                child: Text(
+                  "Create an account?",
+                  style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      backgroundColor: Colors.transparent,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xff586BCA)),
+                ))
+          ],
+        ),
       ),
     );
   }
--- lib/view/Widget/text_field.dart
+++ lib/view/Widget/text_field.dart
@@ -2,71 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TextFieldCustom extends StatelessWidget {
-
   TextFieldCustom({
     Key? key,
     required this.hintTitel,
     required this.errorText,
-    required this.isShow ,
-    required this.prefixIcon,
-    this.safixIcon ,
+    required this.isHide,
+     this.prefixIcon,
+    this.safixIcon,
     required this.textInputType,
     required this.controller,
-
   });
 
   final String? hintTitel;
   final String errorText;
-  bool  isShow ;
-  final Icon prefixIcon;
-  IconButton? safixIcon;
+  bool isHide;
+
+  final Icon ? prefixIcon;
+  Widget? safixIcon;
   final TextInputType textInputType;
   final TextEditingController controller;
-
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
       keyboardType: TextInputType.emailAddress,
-      maxLines:  1,
-      minLines:   1,
-
-
-      obscuringCharacter: ".",
-      obscureText: isShow,
-
+      maxLines: 1,
+      minLines: 1,
+style: GoogleFonts.poppins(height: 2),
+      obscureText: isHide,
       decoration: InputDecoration(
 
         constraints: BoxConstraints(maxHeight: errorText == null ? 50 : 70),
-        hoverColor: Colors.green,
 
-        // */
-
-        suffixIcon: safixIcon,
+        suffix: safixIcon,
 //******************************************************************************************************
         hintText: hintTitel,
-        contentPadding: EdgeInsetsDirectional.only(start: 10),
+        // contentPadding: const EdgeInsetsDirectional.only(start: 10),
         hintMaxLines: 1,
+
         prefixIcon: prefixIcon,
-        prefixIconColor: Color(0xff1B2A3B),
-        suffixIconColor: Color(0xff1B2A3B) ,
+        prefixIconColor: const Color(0xff1B2A3B),
+        suffixIconColor: const Color(0xff1B2A3B),
         focusColor: Colors.white,
         enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 1, color: Colors.grey)),
+            borderSide: const BorderSide(width: 1, color: Colors.grey)),
         focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
             borderSide: BorderSide(
                 width: 1,
                 color: Colors.blue.shade200,
                 style: BorderStyle.solid)),
-        errorText: errorText,
+        // errorText: errorText,
         errorMaxLines: 1,
+
         errorBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: Colors.red.shade300,
+            borderSide: const BorderSide(
+              color: Color(0xff586BCA),
               width: 1,
             )),
         focusedErrorBorder: OutlineInputBorder(
